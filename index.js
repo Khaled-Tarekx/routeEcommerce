f93e08a3c88cb34819dfa8f3cbf45f48bb6b536d
KhaledTarek <khaledtarek258x@gmail.com>
2024-10-09 02:27:19 +0300

    fixed vercel.json

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import 'dotenv/config';
+
 import express from 'express';
 import bootstrap from './src/setup/bootstrap.js';
 import { connection } from './database/connection.js';
 
 const app = express();
+
 const port = process.env.PORT;
+
 connection
 	.then(() => console.log('connented to db successfully'))
 	.catch(() => console.log('connection to db didnt work'));
